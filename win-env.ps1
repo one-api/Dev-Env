9cd262478fe1ab19fb9789833f23e9ae4693d3b0
one-api <57555932+one-api@users.noreply.github.com>
2024-08-17 17:14:58 +0800

    Update win-env.ps1

--- win-env.ps1
+++ win-env.ps1
@@ -1,2 +1,13 @@
-Invoke-WebRequest -UseBasicParsing -Uri "https://go.dev/dl/go1.22.2.windows-amd64.msi"
-msiexec /i go1.22.2.windows-amd64.msi
+URL_Node="https://nodejs.org/dist/v22.6.0/node-v22.6.0-x64.msi"
+URL_Python="https://www.python.org/ftp/python/3.12.5/python-3.12.5-amd64.exe"
+URL_Golang="https://go.dev/dl/go1.22.2.windows-amd64.msi"
+
+URL_Vscode="https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
+URL_JBToolbox="https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.4.2.32922.exe"
+
+URL_Wsl2="https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
+URL_Chrome="https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi"
+
+
+Invoke-WebRequest -UseBasicParsing -Uri 
+msiexec /i "C:\Users\YourUsername\Downloads\example_installer.msi" /quiet /norestart
