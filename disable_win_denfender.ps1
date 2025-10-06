8c687f93d1c7f6e8444fa9582fbbc0002ab9f4ad
one-api <57555932+one-api@users.noreply.github.com>
2025-10-06 15:52:50 +0800

    Delete disable_win_denfender.ps1

--- disable_win_denfender.ps1
+++
@@ -1,3 +0,0 @@
-Disable-ScheduledTask -TaskPath "\Microsoft\Windows\Windows Defender" -TaskName "Windows Defender Scheduled Scan"
-
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1
